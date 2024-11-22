9d07ad9514761dacd196281586c8e06aca1443f5
Denys <feed.exfoss@gmail.com>
2024-11-22 23:21:26 +0200

    calculation change to radians

--- model/circle.cpp
+++ model/circle.cpp
@@ -3,30 +3,34 @@
 Circle::Circle(Point center, double radius, Point* start, Point* end, double degreeStart,
                double degreeEnd) : start(start), end(end), center(center), radius(radius), degreeStart(degreeStart),
                                    degreeEnd(degreeEnd) {
-    computeCircle();
 }
 
 void Circle::computeCircle() {
     lines.clear();
-    for (double i = degreeStart; i < degreeEnd; i++) {
-        double degree0 = i;
-        double degree1 = i + 1;
-        if (i == degreeStart) {
-            start->setX(computePointX(degree0));
-            start->setY(computePointY(degree0));
-            Point point1 = Point(computePointX(degree1), computePointY(degree1));
-            lines.emplace_back(*start, point1);
-        } else if (i == degreeEnd - 1) {
-            Point point0 = Point(computePointX(degree0), computePointY(degree0));
-            end->setX(computePointX(degree1));
-            end->setY(computePointY(degree1));
-            lines.emplace_back(point0,* end);
-        } else {
-            Point point0 = Point(computePointX(degree0), computePointY(degree0));
-            Point point1 = Point(computePointX(degree1), computePointY(degree1));
-            lines.emplace_back(point0, point1);
-        }
+
+    double step = 1.0;
+    double radStart = degreeStart * M_PI / 180.0;
+    double radEnd = degreeEnd * M_PI / 180.0;
+
+    double rad0 = radStart;
+    double rad1 = rad0 + step * M_PI / 180.0;
+
+    Point point0(computePointX(rad0), computePointY(rad0));
+    start->setX(point0.x());
+    start->setY(point0.y());
+
+    while (rad0 < radEnd) {
+        rad1 = std::min(radEnd, rad0 + step * M_PI / 180.0); // Avoid overshooting
+
+        Point point1(computePointX(rad1), computePointY(rad1));
+        lines.emplace_back(point0, point1);
+
+        point0 = point1;
+        rad0 = rad1;
     }
+
+    end->setX(point0.x());
+    end->setY(point0.y());
 }
 
 double Circle::computePointX(double degree) const {
