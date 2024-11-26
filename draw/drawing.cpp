77ee84f904fa0550224fb2e182db69ed4a5c400c
Denys <feed.exfoss@gmail.com>
2024-11-27 00:48:52 +0200

    refactor and insert signal to wheel event

--- draw/drawing.cpp
+++ draw/drawing.cpp
@@ -1,7 +1,5 @@
 #include "drawing.h"
-#include <iostream>
 
-#define log(x) std::cout << x << std::endl;
 
 Draw::Draw(QGraphicsScene* scene)
     : m_grid(new CoordinateGrid()),
@@ -10,6 +8,12 @@ Draw::Draw(QGraphicsScene* scene)
     if (scene) {
         scene->addItem(this);
     }
+
+    m_matrix = new double*[3];
+    for (int i = 0; i < 3; ++i) {
+        m_matrix[i] = new double[3];
+    }
+
     updateScene();
 }
 
@@ -44,6 +48,7 @@ void Draw::wheelEvent(QGraphicsSceneWheelEvent* event) {
         }
     }
     updateScene();
+    emit pixelsPerCmChanged(m_pixelsPerCm);
     event->accept();
 }
 
@@ -60,7 +65,6 @@ void Draw::setGridSize(int size) {
 
 void Draw::setPixelsPerCm(double pixelsPerCm) {
     m_pixelsPerCm = pixelsPerCm;
-    //emit pixelsPerCmChanged(pixelsPerCm);
     updateScene();
 }
 
@@ -69,92 +73,83 @@ void Draw::resetScene() {
 }
 
 void Draw::applyAffineTransformation(double Xx, double Xy, double Yx, double Yy, double Ox, double Oy) {
-    double **matrix = new double*[3];
-    for (int i = 0; i < 3; ++i) {
-        matrix[i] = new double[3];
-    }
-    matrix[0][0] = Xx;
-    matrix[0][1] = Xy;
-    matrix[0][2] = 0;
-    matrix[1][0] = Yx;
-    matrix[1][1] = Yy;
-    matrix[1][2] = 0;
-    matrix[2][0] = Ox * m_pixelsPerCm;
-    matrix[2][1] = Oy * m_pixelsPerCm;
-    matrix[2][2] = 1;
+    m_matrix[0][0] = Xx;
+    m_matrix[0][1] = Xy;
+    m_matrix[0][2] = 0;
+    m_matrix[1][0] = Yx;
+    m_matrix[1][1] = Yy;
+    m_matrix[1][2] = 0;
+    m_matrix[2][0] = Ox * m_pixelsPerCm;
+    m_matrix[2][1] = Oy * m_pixelsPerCm;
+    m_matrix[2][2] = 1;
 
-    m_grid->transform(matrix);
-    m_figure->transform(matrix);
+    m_grid->transform(m_matrix);
+    m_figure->transform(m_matrix);
     update();
 }
 
 void Draw::applyProjectiveTransformation(double Xx, double Xy, double Xw, double Yx, double Yy, double Yw, double Ox, double Oy, double Ow) {
-    double **matrix = new double*[3];
-    for (int i = 0; i < 3; ++i) {
-        matrix[i] = new double[3];
-    }
+    m_matrix[0][0] = Xx * Xw * m_pixelsPerCm;
+    m_matrix[0][1] = Xy * Xw * m_pixelsPerCm;
+    m_matrix[0][2] = Xw;
+    m_matrix[1][0] = Yx * Yw * m_pixelsPerCm;
+    m_matrix[1][1] = Yy * Yw * m_pixelsPerCm;
+    m_matrix[1][2] = Yw;
+    m_matrix[2][0] = Ox * Ow * m_pixelsPerCm;
+    m_matrix[2][1] = Oy * Ow * m_pixelsPerCm;
+    m_matrix[2][2] = Ow;
 
-    matrix[0][0] = Xx * Xw * m_pixelsPerCm;
-    matrix[0][1] = Xy * Xw * m_pixelsPerCm;
-    matrix[0][2] = Xw;
-    matrix[1][0] = Yx * Yw * m_pixelsPerCm;
-    matrix[1][1] = Yy * Yw * m_pixelsPerCm;
-    matrix[1][2] = Yw;
-    matrix[2][0] = Ox * Ow * m_pixelsPerCm;
-    matrix[2][1] = Oy * Ow * m_pixelsPerCm;
-    matrix[2][2] = Ow;
-
-    for (int i=0; i<3; i++) {
-        for (int j=0; j<3; j++) {
-            log(matrix[i][j]);
-        }
-    }
-
-    log ("-----------------");
-    m_grid->transform(matrix);
-    m_figure->transform(matrix);
+    m_grid->transform(m_matrix);
+    m_figure->transform(m_matrix);
     update();
 }
 
 void Draw::applyShiftToFigure(double shiftX, double shiftY) {
-    double **matrix = new double*[3];
-    for (int i = 0; i < 3; ++i) {
-        matrix[i] = new double[3];
-    }
-    matrix[0][0] = 1;
-    matrix[0][1] = 0;
-    matrix[0][2] = 0;
-    matrix[1][0] = 0;
-    matrix[1][1] = 1;
-    matrix[1][2] = 0;
-    matrix[2][0] = shiftX * m_pixelsPerCm;
-    matrix[2][1] = shiftY * m_pixelsPerCm;
-    matrix[2][2] = 1;
+    m_matrix[0][0] = 1;
+    m_matrix[0][1] = 0;
+    m_matrix[0][2] = 0;
+    m_matrix[1][0] = 0;
+    m_matrix[1][1] = 1;
+    m_matrix[1][2] = 0;
+    m_matrix[2][0] = shiftX * m_pixelsPerCm;
+    m_matrix[2][1] = shiftY * m_pixelsPerCm;
+    m_matrix[2][2] = 1;
 
-    m_figure->transform(matrix);
+    m_figure->transform(m_matrix);
     update();
 }
 
 void Draw::applyRotateToFigure(double x, double y, double angle) {
-    double **matrix = new double*[3];
-    for (int i = 0; i < 3; ++i) {
-        matrix[i] = new double[3];
-    }
-
     double rad = angle * M_PI / 180.0;
-    matrix[0][0] = cos(rad);
-    matrix[0][1] = sin(rad);
-    matrix[0][2] = 0;
-    matrix[1][0] = -sin(rad);
-    matrix[1][1] = cos(rad);
-    matrix[1][2] = 0;
-    matrix[2][0] = -x * m_pixelsPerCm * (cos(rad) - 1) + y * m_pixelsPerCm * sin(rad);
-    matrix[2][1] = -x * m_pixelsPerCm * sin(rad) - y * m_pixelsPerCm * (cos(rad) - 1);
-    matrix[2][2] = 1;
+    m_matrix[0][0] = cos(rad);
+    m_matrix[0][1] = sin(rad);
+    m_matrix[0][2] = 0;
+    m_matrix[1][0] = -sin(rad);
+    m_matrix[1][1] = cos(rad);
+    m_matrix[1][2] = 0;
+    m_matrix[2][0] = -x * m_pixelsPerCm * (cos(rad) - 1) + y * m_pixelsPerCm * sin(rad);
+    m_matrix[2][1] = -x * m_pixelsPerCm * sin(rad) - y * m_pixelsPerCm * (cos(rad) - 1);
+    m_matrix[2][2] = 1;
 
-    m_figure->transform(matrix);
+    m_figure->transform(m_matrix);
     update();
 }
+
+void Draw::applySymmetryToFigure(double x, double y) {
+    m_matrix[0][0] = -1;
+    m_matrix[0][1] = 0;
+    m_matrix[0][2] = 0;
+    m_matrix[1][0] = 0;
+    m_matrix[1][1] = -1;
+    m_matrix[1][2] = 0;
+    m_matrix[2][0] = 2 * x * m_pixelsPerCm;
+    m_matrix[2][1] = 2 * y * m_pixelsPerCm;
+    m_matrix[2][2] = 1;
+
+    m_figure->transform(m_matrix);
+    update();
+}
+
 
 
 
